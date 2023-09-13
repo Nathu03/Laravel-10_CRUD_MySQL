18097e63a4fa5c8690f81c676abbaeac560a6408
Nathu03 <Narthanan.s@yahoo.com>
2023-09-13 11:11:32 +0530

    second commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group that
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('products', ProductController::class);
+
+Route::get('/', function () {
+    return redirect()->route('products.index');
+});
+
